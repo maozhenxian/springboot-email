52ad7c25056216d0fa5c5b8e8cfdffddd555da4b
mzx <742204682@qq.com>
2018-11-22 15:29:18 +0800

    this is my first commit about this project

---
+++ src/main/java/com/mzx/SpringbootEmailApplication.java
@@ -0,0 +1,12 @@
+package com.mzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmailApplication.class, args);
+	}
+}
